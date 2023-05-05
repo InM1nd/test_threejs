4777370512544e8f9570e227783b6c035133940e
InM1nd <zabolotny1111@gmail.com>
2023-05-05 23:07:42 +0200

    update

--- src/components/Content/Content.jsx
+++ src/components/Content/Content.jsx
@@ -3,7 +3,6 @@ import {  } from "./Content.styled"
 
 import MenuButton from "./MenuButton/MenuButton"
 import Hero from "./Hero/Hero"
-
 import Contact from "./Contact/Contact"
 import Projects from "./Projects/Projects"
 import About from "./About/About"
