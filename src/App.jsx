a96955fc1ffd9ae1b9e7b0079ba5e66127dcc358
InM1nd <zabolotny1111@gmail.com>
2023-05-05 22:35:44 +0200

    test

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import Header from "./components/Header/Header"
 import Content from "./components/Content/Content"
-import Footer from "./components/footer/footer"
+import Footer from './components/Footer/Footer'
 
 function App() {
   
