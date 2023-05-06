1e3ab6b8ba10be912320daeb3aef3ca985e7bb43
InM1nd <zabolotny1111@gmail.com>
2023-05-06 15:20:00 +0200

    Update Content.jsx

--- src/components/Content/Content.jsx
+++ src/components/Content/Content.jsx
@@ -5,7 +5,7 @@ import MenuButton from "./MenuButton/MenuButton.jsx"
 import Hero from "./Hero/Hero.jsx"
 import Contact from "./Contacts/Contacts.jsx"
 // import Projects from './Projects/Projects.jsx';
-import About from "./About/About.jsx"
+// import About from "./About/About.jsx"
 
 const Content = () => {
     return (
@@ -13,7 +13,7 @@ const Content = () => {
         <MenuButton/>
         <Hero/>
         {/* <Projects/> */}
-        <About/>
+        {/* <About/> */}
         <Contact/>
         </>
     )
